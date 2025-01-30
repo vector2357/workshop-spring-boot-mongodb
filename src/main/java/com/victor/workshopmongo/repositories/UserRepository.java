1e59238a4e2ecc5628357848fa86970852a797ee
Victor Hugo <victorhugorm2004@gmail.com>
2025-01-30 19:18:25 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/victor/workshopmongo/repositories/UserRepository.java
+++ src/main/java/com/victor/workshopmongo/repositories/UserRepository.java
@@ -7,5 +7,4 @@ import com.victor.workshopmongo.domain.User;
 
 @Repository
 public interface UserRepository extends MongoRepository<User, String> {
-
 }
--- src/main/java/com/victor/workshopmongo/services/UserService.java
+++ src/main/java/com/victor/workshopmongo/services/UserService.java
@@ -35,6 +35,17 @@ public class UserService {
 		repo.deleteById(id);
 	}
 	
+	public User update(User obj) {
+		User newObj = this.findById(obj.getId());
+		updateData(newObj, obj);
+		return repo.save(newObj);
+	}
+	
+	private void updateData(User newObj, User obj) {
+		newObj.setName(obj.getName());
+		newObj.setEmail(obj.getEmail());
+	}
+
 	public User fromDTO(UserDTO objDto) {
 		return new User(objDto.id(), objDto.name(), objDto.email());
 	}
